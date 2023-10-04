6add159fcc5b923fd0ee8f28c0c227f9faff3b11
Zenodesp <Zeno.despiegelaere@student.ehb.be>
2023-10-04 09:53:51 +0200

    Add project files.

---
+++ ClassesEvents/Boek.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesEvents
+{
+
+
+
+    public enum Verschijningsperiode
+    {
+        Dagelijks,
+        Wekelijks,
+        Maandelijks
+    }
+    internal class Boek
+    {
+        public static int Volgnummer { get { id++; return id; } set { id = value; } }
+        public int Isbn { get; set; }
+        public string Naam { get; set; }
+        public string Uitgever { get; set; }
+        public double Prijs { get {return prijs; } set { if (value < 50.00 && value > 5.00) { prijs = value; } else {throw new ArgumentOutOfRangeException("geen geldige prijs"); } } }
+
+        private static int id;
+        private static double prijs;
+
+        public Boek()
+        {
+            Isbn = Volgnummer;
+        }
+
+        public Boek(string Naam, string Uitgever, double Prijs)
+        {
+            Isbn = Volgnummer;
+            this.Naam = Naam;
+            this.Prijs = Prijs;
+            this.Uitgever = Uitgever;
+        }
+
+        public void Lees(string Naam)
+        {
+             Console.WriteLine(Naam + " is gelezen!");
+        }
+
+        public override string ToString()
+        {
+            return Isbn + " - " + Naam + " - " + Uitgever + " - " + Prijs + " - ";
+        }
+    }
+
+    internal class Tijdschrift : Boek
+    {
+        public Verschijningsperiode vp;
+
+        public Tijdschrift( string Naam, string Uitgever, double Prijs, Verschijningsperiode vp) {
+            Isbn = Volgnummer;
+            this.Naam = Naam;
+            this.Prijs = Prijs;
+            this.Uitgever = Uitgever;
+            this.vp = vp;
+        }
+
+    }
+
+    internal class Bestelling<T>
+    {
+        public event EventHandler BestellingMaken;
+
+        void AlsBestelling(string message)
+        {
+            if (BestellingMaken != null)
+            {
+                BestellingMaken(message);
+            }
+        }
+        public static int Volgnummer { get { id++; return id; } set { id = value; } }
+        public int Id { get; set; }
+        public Boek Item { get; set; }
+        public DateTime Datum { get; set; }
+        public int Aantal { get; set; }
+        public Verschijningsperiode Abonnement { get; set; }
+
+        public static void message(string message)
+        {
+            Console.WriteLine("Report: " + message);
+        }
+
+        private static int id;
+
+
+        public Bestelling()
+        {
+            BestellingMaken += message;
+        }
+        public Tuple<int, int, double> Bestel<T>(Boek bb)
+        {
+            Console.WriteLine("Hoeveel boeken wilt u aankopen?");
+            Aantal = Convert.ToInt32(Console.ReadLine());
+
+
+            Tuple<int, int, double> resultaat = Tuple.Create
+            (
+                bb.Isbn,
+                 Aantal,
+               (bb.Prijs * Aantal)
+            );
+            Item = bb;
+            Datum = DateTime.Now;
+
+
+            AlsBestelling("Bestelling gemaakt!");
+
+            return resultaat;
+
+            
+        }
+
+    }
+
+    
+}
---
+++ ClassesEvents/Program.cs
@@ -0,0 +1,76 @@
+﻿using ClassesEvents;
+
+delegate void EventHandler(string message);
+internal class Program
+{
+
+    public static void message(string message)
+    {
+        Console.WriteLine("Report: " + message);
+    }
+
+    private static void Main(string[] args)
+    {
+        char keuze = '0';
+
+        string naam, uitgever;
+        double prijs;
+        Verschijningsperiode per;
+
+        Boek b1 = new Boek();
+
+        do
+        {
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("1 Geef een boek in");
+            Console.WriteLine("2 Geef een tijdschrift in");
+            Console.WriteLine("3 Bestel een boek of tijdschrift");
+            keuze = Console.ReadLine()[0];
+            Console.WriteLine();
+
+            switch (keuze) { 
+                case '0':
+                    break;
+                case '1':
+                    Console.WriteLine("Geef de naam van het boek in.");
+                    naam = Console.ReadLine();
+                    Console.WriteLine("geef de uitgever van het boek in.");
+                    uitgever = Console.ReadLine();
+                    Console.WriteLine("geef de prijs van het boek in.");
+                    prijs = Convert.ToDouble(Console.ReadLine());
+                    b1 = new Boek(naam, uitgever, prijs);
+                    break;
+                case '2':
+                    Console.WriteLine("Geef de naam van het tijdschrift in.");
+                    naam = Console.ReadLine();
+                    Console.WriteLine("geef de uitgever van het tijdschrift in.");
+                    uitgever = Console.ReadLine();
+                    Console.WriteLine("geef de prijs van het tijdschrift in.");
+                    prijs = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("hoe vaak wordt dit tijdschrift uitgegeven?");
+                    Console.WriteLine("1. Dagelijks");
+                    Console.WriteLine("2. Wekelijks");
+                    Console.WriteLine("3. Maandelijks");
+                    char k2 = Console.ReadLine()[0];
+                    if(k2 == '1')
+                    {
+                        per = Verschijningsperiode.Dagelijks;
+                    } else if (k2 == '2')
+                    {
+                        per = Verschijningsperiode.Wekelijks;
+                    } else
+                    {
+                        per = Verschijningsperiode.Maandelijks;
+                    }
+                    Tijdschrift t1 = new Tijdschrift(naam, uitgever, prijs, per);
+                    break;
+                case '3':
+
+                    Bestelling<Boek> bestellingboek = new Bestelling<Boek>();
+                    bestellingboek.Bestel<Boek>(b1);
+                   break;
+            }
+
+        } while (keuze != '0');
+    }
+}
